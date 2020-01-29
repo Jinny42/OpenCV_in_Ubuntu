5cca228461a85f2781ba959c4904d67b8562033b
Jinny42 <47097413+Jinny42@users.noreply.github.com>
2020-01-30 01:17:03 +0900

    Update 00_OpenCV_TEST_SINGLE.py

--- 4day_course/Python/00_OpenCV_TEST_SINGLE.py
+++ 4day_course/Python/00_OpenCV_TEST_SINGLE.py
@@ -1,3 +1,3 @@
 from OpenCV_Functions import *
 
-processingSingleImage("/home/opencv/OpenCV_in_Ubuntu/Data/Lane_Detection_Images/solidWhiteCurve.jpg")
+processingSingleImage("/home/opencv/OpenCV_in_Ubuntu/Data/TrafficLight_Detection/green_light_01.png")
